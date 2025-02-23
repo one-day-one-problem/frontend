611bb39af815b5f559158d4f88238e343fa6e723
namsh1125 <namsh1125@naver.com>
2025-02-23 22:21:16 +0900

    ✨ 랜딩 페이지 구현

---
+++ constants/landing/testimonials.js
@@ -0,0 +1,16 @@
+export const TESTIMONIALS = [
+  {
+    name: "남O현",
+    role: "백엔드 개발자",
+    company: "카카오페이",
+    content: "조만간 갑니다. 카카오페이",
+    avatar: "/avatars/user1.png",
+  },
+  {
+    name: "남O현",
+    role: "프론트엔드 개발자",
+    company: "토스",
+    content: "조만간 갑니다. 토스",
+    avatar: "/avatars/user1.png",
+  },
+];
