b783d4dac604705d4186c568edab507924a20ef0
namsh1125 <namsh1125@naver.com>
2025-03-29 01:14:12 +0900

    ✨ 로그인 페이지 라우트 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
