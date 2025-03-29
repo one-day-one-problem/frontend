bf219acd9173af172fecdfee59febdc06244707b
namsh1125 <namsh1125@naver.com>
2025-03-29 20:20:46 +0900

    ✨ OAuth 콜백 페이지 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
+import OAuthCallbackPage from "./pages/OAuthCallbackPage";
+import { AuthProvider } from "./contexts/AuthContext";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/oauth/callback" element={<OAuthCallbackPage />} />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
