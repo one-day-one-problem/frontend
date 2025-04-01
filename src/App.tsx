aef2ce39b6a3e0ce79f75a94cb8ed6ef026e6ee6
namsh1125 <namsh1125@naver.com>
2025-04-01 10:32:14 +0900

    ✨ 문제 상세 페이지 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import OAuthCallbackPage from "./pages/OAuthCallbackPage";
 import { AuthProvider } from "./contexts/AuthContext";
 import LandingPage from "./pages/LandingPage";
 import ProblemsPage from "./pages/ProblemsPage";
+import ProblemPage from "./pages/ProblemPage";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/oauth/callback" element={<OAuthCallbackPage />} />
           <Route path="/problems" element={<ProblemsPage />} />
+          <Route path="/problems/:problemId" element={<ProblemPage />} />
         </Routes>
       </AuthProvider>
     </Router>
