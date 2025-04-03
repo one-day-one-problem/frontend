1aba0cf4ace3ac5c353e442e41052138f051242c
SeungHyeon <namsh1125@naver.com>
2025-04-04 07:56:27 +0900

    Revert "✨ 로그인된 사용자가 랜딩 페이지 접근 시 문제 목록 페이지로 리다이렉트"

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,23 @@
-import React, { useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useLocation,
-  useNavigate,
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import OAuthCallbackPage from "./pages/OAuthCallbackPage";
-import { AuthProvider, useAuth } from "./contexts/AuthContext";
+import { AuthProvider } from "./contexts/AuthContext";
 import LandingPage from "./pages/LandingPage";
 import ProblemsPage from "./pages/ProblemsPage";
 import ProblemPage from "./pages/ProblemPage";
-
-function InnerApp() {
-  const { isAuthenticated } = useAuth();
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    // 로그인된 사용자가 랜딩 페이지에 접근하면 문제 목록 페이지로 리다이렉트
-    if (isAuthenticated && location.pathname === "/") {
-      navigate("/problems", { replace: true });
-    }
-  }, [isAuthenticated, location.pathname, navigate]);
-
-  return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/oauth/callback" element={<OAuthCallbackPage />} />
-      <Route path="/problems" element={<ProblemsPage />} />
-      <Route path="/problems/:problemId" element={<ProblemPage />} />
-    </Routes>
-  );
-}
 
 function App() {
   return (
     <Router>
       <AuthProvider>
-        <InnerApp />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/oauth/callback" element={<OAuthCallbackPage />} />
+          <Route path="/problems" element={<ProblemsPage />} />
+          <Route path="/problems/:problemId" element={<ProblemPage />} />
+        </Routes>
       </AuthProvider>
     </Router>
   );
